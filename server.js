5ab85ac0ac233f9f30aa9583f2d45056ae568c38
sugam9 <agarwalsugam.99@gmail.com>
2017-08-26 21:18:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -105,11 +105,11 @@ app.get('/articleName',function(req,res){
 });
 
 app.get('/article-two',function(req,res){
- res.send(createTemplate(articleTwo));
+res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
 app.get('/article-three',function(req,res){
- res.send(createTemplate(articleThree));
+ res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
